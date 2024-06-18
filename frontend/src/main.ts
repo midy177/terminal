d7e4b88e471eed1dd9d1148a1da20fbe949d394f
Your Name <your@example.com>
2024-06-18 13:35:18 +0800

    全局注册STable

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import DevUI from 'vue-devui';
+import STable from '@shene/table';
 import '@shene/table/dist/index.css';
 import './style.css';
 import 'vue-devui/style.css';
@@ -9,6 +10,5 @@ import { ThemeServiceInit, galaxyTheme } from 'devui-theme';
 const themeService = ThemeServiceInit({ galaxyTheme }, 'galaxyTheme');
 // 可以动态切换成 galaxyTheme 追光主题
 themeService?.applyTheme(galaxyTheme);
-// import STable from '@shene/table';
 
-createApp(App).use(DevUI).mount('#app')
+createApp(App).use(STable).use(DevUI).mount('#app')
