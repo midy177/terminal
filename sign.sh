aad2b58f093ba9524505a41a5214ac6487ce062c
medy <1228022817@qq.com>
2024-06-22 20:44:04 +0800

    添加mac打包命令

--- sign.sh
+++ sign.sh
@@ -10,3 +10,14 @@ read is_sign
 if [[ $is_sign  = "y" ]]; then
   spctl --verbose=4 --assess --type execute build/bin/terminal.app
 fi
+
+cd build/bin
+create-dmg --volname terminal \
+           --window-pos 200 120 \
+           --window-size 800 400 \
+           --icon-size 100 \
+           --icon terminal.app 200 190 \
+           --hide-extension terminal.app \
+           --app-drop-link  600 185 \
+           terminal_setup.dmg \
+           terminal.app
