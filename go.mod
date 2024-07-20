b742c841434d385af96be3a77d5dd01aa1613c8f
medy <1228022817@qq.com>
2024-07-20 09:36:29 +0800

    chore: 使用runtime/pprof调试性能

--- go.mod
+++ go.mod
@@ -13,7 +13,6 @@ require (
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/inhies/go-bytesize v0.0.0-20220417184213-4913239db9cf
 	github.com/klauspost/compress v1.17.9
-	github.com/labstack/echo/v4 v4.12.0
 	github.com/lucasb-eyer/go-colorful v1.2.0
 	github.com/mattn/go-runewidth v0.0.15
 	github.com/mattn/go-sqlite3 v1.14.22
@@ -52,6 +51,7 @@ require (
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
 	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
 	github.com/kr/fs v0.1.0 // indirect
+	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leaanthony/go-ansi-parser v1.6.0 // indirect
 	github.com/leaanthony/gosod v1.0.3 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"embed"
-	"github.com/labstack/echo/v4"
+	"flag"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/wailsapp/wails/v2/pkg/application"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -12,10 +13,10 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
-	"net/http"
-	"net/http/pprof"
+	"log"
+	"os"
+	"runtime/pprof"
 	"terminal/logic"
-	"time"
 )
 
 //go:embed all:frontend/dist
@@ -77,7 +78,7 @@ func main() {
 			ProgramName:         "terminal-console",
 		},
 	})
-	//go startPprof()
+	//startPprof()
 	err := app.Run()
 	if err != nil {
 		println("Error:", err.Error())
@@ -85,30 +86,16 @@ func main() {
 }
 
 func startPprof() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.HideBanner = true
-	RegisterRoutes(e)
-	e.Logger.Fatal(e.StartServer(&http.Server{
-		Addr:              ":1323",
-		ReadTimeout:       time.Second * 5,
-		ReadHeaderTimeout: time.Second * 2,
-		WriteTimeout:      time.Second * 120,
-	}))
-}
-func RegisterRoutes(engine *echo.Echo) {
-	router := engine.Group("/debug")
-	// 下面的路由根据要采集的数据需求注册，不用全都注册
-	router.GET("/pprof", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
-	router.GET("/pprof/allocs", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
-	router.GET("/pprof/block", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
-	router.GET("/pprof/goroutine", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
-	router.GET("/pprof/heap", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
-	router.GET("/pprof/mutex", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
-	router.GET("/pprof/cmdline", echo.WrapHandler(http.HandlerFunc(pprof.Cmdline)))
-	router.GET("/pprof/profile", echo.WrapHandler(http.HandlerFunc(pprof.Profile)))
-	router.GET("/pprof/symbol", echo.WrapHandler(http.HandlerFunc(pprof.Symbol)))
-	router.GET("/pprof/trace", echo.WrapHandler(http.HandlerFunc(pprof.Trace)))
+	enablePprof := flag.Bool("pprof", false, "enable pprof")
+	flag.Parse()
+	if *enablePprof {
+		fmt.Println("starting pprof")
+		f, _ := os.OpenFile("cpu.pprof", os.O_CREATE|os.O_RDWR, 0644)
+		defer f.Close()
+		err := pprof.StartCPUProfile(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 }
